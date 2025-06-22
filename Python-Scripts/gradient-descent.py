22cd2596c6b6702eec82c7180187419ec3063ebf
Jagath Kiran <jagathkiran14569@gmail.com>
2025-06-22 21:02:11 +0530

    Functions

---
+++ Python-Scripts/gradient-descent.py
@@ -0,0 +1,6 @@
+for i in range(epochs): 
+    Y_pred = m*X + c  # The current predicted value of Y
+    D_m = (-2/n) * sum(X * (Y - Y_pred))  # Derivative wrt m
+    D_c = (-2/n) * sum(Y - Y_pred)  # Derivative wrt c
+    m = m - L * D_m  # Update m
+    c = c - L * D_c  # Update c  
